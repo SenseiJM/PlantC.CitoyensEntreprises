424d2d1232ac267fb5d7be47543a176e86462d57
Jean-Michel <loudeche.jean-michel@hotmail.fr>
2021-11-18 12:20:39 +0100

    Added Projet File and linked Enums to work on

---
+++ PlantC.CitoyensEntreprise.DAL/Entities/Projet.cs
@@ -0,0 +1,28 @@
+﻿using PlantC.CitoyensEntreprise.DAL.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantC.CitoyensEntreprise.DAL.Entities
+{
+    public class Projet {
+
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        public string Localite { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public TypeProjet TypeDeProjet { get; set; }
+        public StatutProjet Statut { get; set; }
+        public ushort CodePostal { get; set; }
+        public string Description { get; set; }
+        public uint NbArbresTotal { get; set; }
+        public uint NbParticipantsTotal { get; set; }
+        public double SommeRecoltee { get; set; }
+        public double ObjectifMonetaire { get; set; }
+        public uint ObjectifArbres { get; set; }
+
+    }
+}
---
+++ PlantC.CitoyensEntreprise.DAL/Enums/TypeProjet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantC.CitoyensEntreprise.DAL.Enums {
+    public enum TypeProjet {
+        Type1,
+        Type2,
+        Type3,
+        Type4,
+        Type5
+    }
+}
