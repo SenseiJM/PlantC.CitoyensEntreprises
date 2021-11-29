30da393a2b2568438259d85619b6ef525c025560
PetrovC <constantin.p05@gmail.com>
2021-11-29 10:19:58 +0100

    test

--- PlantC.CitoyensEntreprise.DAL/Repositories/AlbumRepository.cs
+++ PlantC.CitoyensEntreprise.DAL/Repositories/AlbumRepository.cs
@@ -2,9 +2,6 @@ using Npgsql;
 using PlantC.CitoyensEntreprise.DAL.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Task;
 
 namespace PlantC.CitoyensEntreprise.DAL.Repositories
 {
@@ -52,7 +49,7 @@ namespace PlantC.CitoyensEntreprise.DAL.Repositories
                 while (reader.Read())
                 {
                     result.Add(new Album
-                    {   
+                    {
                         Id = (int)reader["Id"],
                         ProjetId = (int)reader["ProjetId"],
                         URLPhoto = reader["URLPhoto"].ToString(),
@@ -78,40 +75,52 @@ namespace PlantC.CitoyensEntreprise.DAL.Repositories
         /// </summary>
         /// <param name="id">ID to be searched</param>
         /// <returns>Returns corresponding Album Entity</returns>
-        public Album GetByID(int id) {
-            try {
+        public Album GetByID(int id)
+        {
+            try
+            {
                 oConn.Open();
                 NpgsqlCommand cmd = oConn.CreateCommand();
                 cmd.CommandText = "SELECT * Album Task WHERE Id = @p1";
                 cmd.Parameters.AddWithValue("p1", id);
                 NpgsqlDataReader reader = cmd.ExecuteReader();
                 Album t = null;
-                if (reader.Read()) {
-                    t = new Album {
+                if (reader.Read())
+                {
+                    t = new Album
+                    {
                         Id = (int)reader["Id"],
                         ProjetId = (int)reader["ProjetId"],
                         URLPhoto = reader["URLPhoto"].ToString(),
                         IsPublic = (bool)reader["IsPublic"]
                     };
                     return t;
-                } else {
+                }
+                else
+                {
                     return null;
                 }
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 throw;
-            } finally {
+            }
+            finally
+            {
                 oConn.Close();
             }
         }
 
-        
+
         /// Searches the database to update the Album corresponding to the ID
         /// </summary>
         /// <param name="id">ID to be updated</param>
         /// <param name="c">Album Entity to be updated</param>
         /// <returns>True if Album Entity has been updated, False if ID is not existing</returns>
-        public bool Update(int id, Album c) {
-            try {
+        public bool Update(int id, Album c)
+        {
+            try
+            {
                 oConn.Open();
                 NpgsqlCommand cmd = oConn.CreateCommand();
                 cmd.CommandText = "UPDATE Album SET" +
@@ -125,9 +134,13 @@ namespace PlantC.CitoyensEntreprise.DAL.Repositories
                 cmd.Parameters.AddWithValue("p4", c.IsPublic);
 
                 return cmd.ExecuteNonQuery() != 0;
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 throw;
-            } finally {
+            }
+            finally
+            {
                 oConn.Close();
             }
         }
@@ -138,16 +151,22 @@ namespace PlantC.CitoyensEntreprise.DAL.Repositories
         /// </summary>
         /// <param name="id">ID to be deleted</param>
         /// <returns>True if Album Entity has been deleted, False if ID is not existing</returns>
-        public bool Delete(int id) {
-            try {
+        public bool Delete(int id)
+        {
+            try
+            {
                 oConn.Open();
                 NpgsqlCommand cmd = oConn.CreateCommand();
                 cmd.CommandText = "DELETE FROM Album WHERE Id = @p1";
                 cmd.Parameters.AddWithValue("p1", id);
                 return cmd.ExecuteNonQuery() != 0;
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 throw;
-            } finally {
+            }
+            finally
+            {
                 oConn.Close();
             }
         }
