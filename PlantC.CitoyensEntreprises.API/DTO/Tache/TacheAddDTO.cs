1f3742862e2ad99792fa0b8957ece480059d138d
Constantin Petrov <constantin.p05@gmail.com>
2021-12-01 14:13:20 +0100

    tache end point post

--- PlantC.CitoyensEntreprises.API/DTO/Tache/TacheAddDTO.cs
+++ PlantC.CitoyensEntreprises.API/DTO/Tache/TacheAddDTO.cs
@@ -1,6 +1,17 @@
-﻿namespace PlantC.CitoyensEntreprises.API.DTO.Tache
+﻿using System;
+
+namespace PlantC.CitoyensEntreprises.API.DTO.Tache
 {
     public class TacheAddDTO
     {
+        public int Id_Participant { get; set; }
+        public string Intitule { get; set; }
+        public DateTime Date_Debut { get; set; }
+        public DateTime Date_Fin { get; set; }
+        public int Id_Projet { get; set; }
+        public string Type { get; set; }
+        public int Id_localisation { get; set; }
+        public string Description { get; set; }
     }
+
 }
