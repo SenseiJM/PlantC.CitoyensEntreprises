a73c38a6af276a80eff95a04ccbcb23512f92636
Jean-Michel <loudeche.jean-michel@hotmail.fr>
2021-12-14 15:33:31 +0100

    Added Nominatim API to get geocode by address

--- PlantC.CitoyensEntreprise.DAL/Repositories/LocalisationRepository.cs
+++ PlantC.CitoyensEntreprise.DAL/Repositories/LocalisationRepository.cs
@@ -1,16 +1,21 @@
-﻿using Npgsql;
+﻿using Newtonsoft.Json;
+using Npgsql;
+using PlantC.CitoyensEntreprise.DAL.Entities;
 using PlantC.CitoyensEntreprise.DAL.Entities.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Encodings.Web;
 
 namespace PlantC.CitoyensEntreprise.DAL.Repositories {
     public class LocalisationRepository {
 
         private NpgsqlConnection oConn;
-
-        public LocalisationRepository(NpgsqlConnection oConn) {
+        private readonly HttpClient _client;
+        public LocalisationRepository(NpgsqlConnection oConn, HttpClient client) {
             this.oConn = oConn;
+            _client = client;
         }
 
         public IEnumerable<LocalisationZipView> GetAllLocalisationByZip() {
@@ -41,6 +46,18 @@ namespace PlantC.CitoyensEntreprise.DAL.Repositories {
                 oConn.Close();
             }
 
+        }
+
+        public LocalisationGeoCode GetGeocodeByAddress(string adresse) {
+  
+            adresse = adresse.Replace(" ", "%20");
+            _client.DefaultRequestHeaders.Add("User-Agent", "Other");
+            HttpResponseMessage message = _client.GetAsync("/search?street=" + adresse + "&format=json").Result;
+            if (message.IsSuccessStatusCode) {
+                string json = message.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<List<LocalisationGeoCode>>(json).FirstOrDefault();
+            }
+            throw new HttpRequestException();
         }
 
     }
---
+++ PlantC.CitoyensEntreprises.BLL/Models/LocalisationGeoCodeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantC.CitoyensEntreprises.BLL.Models {
+    public class LocalisationGeoCodeModel {
+
+        public string DisplayName { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+
+    }
+}
